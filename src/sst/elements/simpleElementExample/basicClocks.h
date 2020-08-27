587bb125eddb7ddf003819441f2610f2f377fb8f
Gwen <grvosku@sandia.gov>
2020-08-27 09:58:14 -0600

    simElemEx: First part of re-write

---
+++ src/sst/elements/simpleElementExample/basicClocks.h
@@ -0,0 +1,115 @@
+// Copyright 2009-2020 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2020, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef _BASICCLOCKS_H
+#define _BASICCLOCKS_H
+
+/*
+ * This example demonstrates different ways of managing links
+ *  1. Event sizes are randomly selected between 0 and eventSize for each event
+ *  2. The component uses a Statistic to count the number of payload bytes it received
+ *
+ * Concepts covered:
+ *  - Clocks and clock handlers
+ *  - TimeConverters
+ *  - UnitAlgebra for error checking
+ */
+
+#include <sst/core/component.h>
+
+namespace SST {
+namespace simpleElementExample {
+
+
+// Components inherit from SST::Component
+class basicClocks : public SST::Component
+{
+public:
+
+/*
+ *  SST Registration macros register Components with the SST Core and 
+ *  document their parameters, ports, etc.
+ *  SST_ELI_REGISTER_COMPONENT is required, the documentation macros
+ *  are only required if relevant
+ */
+    // REGISTER THIS COMPONENT INTO THE ELEMENT LIBRARY
+    SST_ELI_REGISTER_COMPONENT(
+        basicClocks,                        // Component class
+        "simpleElementExample",             // Component library (for Python/library lookup)
+        "basicClocks",                      // Component name (for Python/library lookup)
+        SST_ELI_ELEMENT_VERSION(1,0,0),     // Version of the component (not related to SST version)
+        "Basic: managing clocks example",   // Description
+        COMPONENT_CATEGORY_UNCATEGORIZED    // Category
+    )
+
+    // Document the parameters that this component accepts
+    // { "parameter_name", "description", "default value or NULL if required" }
+    SST_ELI_DOCUMENT_PARAMS(
+        { "clock0",     "Frequency (with units) of clock0",     "1GHz" },
+        { "clock1",     "Frequency (with units) of clock1",     "650MHz" },
+        { "clock2",     "Frequency (with units) of clock2",     "1GHz" },
+        { "clockTicks", "Number of clock0 ticks to execute",    "100000" }
+    )
+
+    // Optional since there is nothing to document
+    SST_ELI_DOCUMENT_PORTS()
+    
+    // Optional since there is nothing to document
+    SST_ELI_DOCUMENT_STATISTICS()
+
+    // Optional since there is nothing to document - see SubComponent examples for more info
+    SST_ELI_DOCUMENT_SUBCOMPONENT_SLOTS( )
+
+// Class members
+
+    // Constructor. Components receive a unique ID and the set of parameters that were assigned in the Python input.
+    basicClocks(SST::ComponentId_t id, SST::Params& params);
+    
+    // Destructor
+    ~basicClocks();
+
+private:
+   
+    // Clock handlers for our three clocks
+    virtual bool clock0Tick(SST::Cycle_t cycles);
+    virtual bool clock1Tick(SST::Cycle_t cycles, uint32_t num);
+    virtual bool clock2Tick(SST::Cycle_t cycles, uint32_t num);
+
+    TimeConverter* tc;                  // Timeconverter for clock2
+    Clock::HandlerBase* clock2Handler; // Clock2 handler (clock2Tick)
+
+    void handleEvent(SST::Event *ev);
+
+    // Event handler, called when an event is received on one of link vector elements
+    void handleEventWithID(SST::Event *ev, int linknum);
+
+    // Clock handler, called on each clock cycle
+    virtual bool clockTic(SST::Cycle_t);
+
+    // Params
+    Cycles_t clockCount;
+    std::string clock0Freq;
+    std::string clock1Freq;
+    std::string clock2Freq;
+
+    // SST Output object, for printing, error messages, etc.
+    SST::Output* out;
+     
+};
+
+} // namespace simpleElementExample
+} // namespace SST
+
+#endif /* _BASICCLOCKS_H */
